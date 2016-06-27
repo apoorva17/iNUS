ae00ee0125ae3106c5b7fd719185d77c94105359
apoorva17 <apoorva_ullas@yahoo.com.sg>
2016-06-27 18:49:24 +0800

    Update form.js

--- iNUS/menu/static/js/form.js
+++ iNUS/menu/static/js/form.js
@@ -1,4 +1,13 @@
- function openNav() {
+$(function() {
+   $("li").click(function() {
+      // remove classes from all
+      $("li").removeClass("active");
+      // add class to the one we clicked
+      $(this).addClass("active");
+   });
+})
+
+function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
 }
