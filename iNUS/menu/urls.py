cc02e37ca1d16f82dee7eaf8b3586326ad522413
apoorva17 <apoorva_ullas@yahoo.com.sg>
2016-07-16 16:33:39 +0800

    Update urls.py

--- iNUS/menu/urls.py
+++ iNUS/menu/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 app_name = 'menu'
 urlpatterns = [
- 	 url(r'^$', views.index, name='index'),
-
+ 	 url(r'^year1/$', views.index, name='year1'),
+ 	 url(r'^year2/$', views.index, name='year2'),
+ 	  url(r'^year3/$', views.index, name='year3'),
+ 	 url(r'^year4/$', views.index, name='year4'),
+ 	  url(r'^year5/$', views.index, name='year5'),
+ 	 url(r'^year6/$', views.index, name='year6'),
+ 	 url(r'^module/$', views.module, name='module'),
 ]
