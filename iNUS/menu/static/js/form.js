626d47509ab240cbaabfa9b0d8de2316c5d9f502
apoorva17 <apoorva_ullas@yahoo.com.sg>
2016-07-22 19:04:56 +0800

    Update form.js

--- iNUS/menu/static/js/form.js
+++ iNUS/menu/static/js/form.js
@@ -12,7 +12,7 @@ function addRow(i) {
         td1.innerHTML = document.getElementById("inp1").value;
         td2.innerHTML  = document.getElementById("inp2").value;
         td3.innerHTML  = document.getElementById("inp3").value;
-        td4.innerHTML = '<a onclick = "deleteRow(this)"> <span class="glyphicon glyphicon-remove-sign"></span></a>';
+        td4.innerHTML = '<a onclick = "deleteRow(this)"> <span class="glyphicon glyphicon-remove-sign" id = "remove1"></span></a>';
 
         row.appendChild(td1);
         row.appendChild(td2);
@@ -37,7 +37,7 @@ function addRow(i) {
         td1.innerHTML = document.getElementById("inp4").value;
         td2.innerHTML  = document.getElementById("inp5").value;
         td3.innerHTML  = document.getElementById("inp6").value;
-        td4.innerHTML = '<a onclick = "deleteRow(this)"> <span class="glyphicon glyphicon-remove-sign"></span></a>';
+        td4.innerHTML = '<a onclick = "deleteRow(this)"> <span class="glyphicon glyphicon-remove-sign" id = "remove2"></span></a>';
 
         row.appendChild(td1);
         row.appendChild(td2);
@@ -63,7 +63,7 @@ function addRow(i) {
         td1.innerHTML = document.getElementById("inp7").value;
         td2.innerHTML  = document.getElementById("inp8").value;
         td3.innerHTML  = document.getElementById("inp9").value;
-        td4.innerHTML = '<a onclick = "deleteRow(this)"> <span class="glyphicon glyphicon-remove-sign"></span></a>';
+        td4.innerHTML = '<a onclick = "deleteRow(this)"> <span class="glyphicon glyphicon-remove-sign" id = "remove3"></span></a>';
 
         row.appendChild(td1);
         row.appendChild(td2);
@@ -89,7 +89,7 @@ function addRow(i) {
         td1.innerHTML = document.getElementById("inp10").value;
         td2.innerHTML  = document.getElementById("inp11").value;
         td3.innerHTML  = document.getElementById("inp12").value;
-        td4.innerHTML = '<a onclick = "deleteRow(this)"> <span class="glyphicon glyphicon-remove-sign"></span></a>';
+        td4.innerHTML = '<a onclick = "deleteRow(this)"> <span class="glyphicon glyphicon-remove-sign" id = "remove4"></span></a>';
 
         row.appendChild(td1);
         row.appendChild(td2);
@@ -116,10 +116,28 @@ function deleteRow(node){
 
 
 //Error handling
-function confirmInput() {
-    fname = document.forms[0].inp1.value.length;
-    if(fname == 0){
-        alert("Module code is missing!!")
+function confirmInput1() {
+    input1 = document.forms[0].inp1.value.length;
+    if(input1 == 0){
+        alert("Module code for Semester 1 is missing! Please enter Module Code! :)")
+    }
+}
+function confirmInput4() {
+    input4 = document.forms[1].inp4.value.length;
+    if(input4 == 0){
+        alert("Module code for Semester 2 is missing! Please enter Module Code! :)")
+    }
+}
+function confirmInput7() {
+    input7 = document.forms[2].inp7.value.length;
+    if(input7 == 0){
+        alert("Module code for Special Term 1 is missing! Please enter Module Code! :)")
+    }
+}
+function confirmInput10() {
+    input10 = document.forms[3].inp10.value.length;
+    if(input10 == 0){
+        alert("Module code for Special Term 2 is missing! Please enter Module Code! :)")
     }
 }
 
@@ -131,6 +149,7 @@ $(document).ready(function(){
     $clone1.find("button").attr("onclick", "addRow(2)");
     $clone1.find("table").attr("id", "table1");
     $clone1.find("input[type = text]").attr("id", "inp4");
+    $clone1.find("button").attr("onmousedown", "confirmInput4()");
     $clone1.find("select[name = text]").attr("id", "inp5");
     $clone1.find("select[name = number]").attr("id", "inp6");
 
@@ -139,6 +158,7 @@ $(document).ready(function(){
     $clone2.find("button").attr("onclick", "addRow(3)");
     $clone2.find("table").attr("id", "table2");
     $clone2.find("input[type = text]").attr("id", "inp7");
+    $clone2.find("button").attr("onmousedown", "confirmInput7()");
     $clone2.find("select[name = text]").attr("id", "inp8");
     $clone2.find("select[name = number]").attr("id", "inp9");
     
@@ -148,6 +168,7 @@ $(document).ready(function(){
     $clone3.find("button").attr("onclick", "addRow(4)");
     $clone3.find("table").attr("id", "table3");
     $clone3.find("input[type = text]").attr("id", "inp10");
+    $clone3.find("button").attr("onmousedown", "confirmInput10()");
     $clone3.find("select[name = text]").attr("id", "inp11");
     $clone3.find("select[name = number]").attr("id", "inp12");
     
