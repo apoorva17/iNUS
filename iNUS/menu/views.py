a19c8433a4e11a34f3f12b8f2820e0437453c696
apoorva17 <apoorva_ullas@yahoo.com.sg>
2016-08-10 15:11:34 +0800

    Update views.py

--- iNUS/menu/views.py
+++ iNUS/menu/views.py
@@ -2,8 +2,28 @@ from django.http import HttpResponse
 from .models import Sem, Modules
 from django.template import loader
 
-def index(request):
-	template = loader.get_template('menu/index.html')
+def index1(request):
+	template = loader.get_template('menu/index1.html')
+	return HttpResponse(template.render(request))
+
+def index2(request):
+	template = loader.get_template('menu/index2.html')
+	return HttpResponse(template.render(request))
+
+def index3(request):
+	template = loader.get_template('menu/index3.html')
+	return HttpResponse(template.render(request))
+
+def index4(request):
+	template = loader.get_template('menu/index4.html')
+	return HttpResponse(template.render(request))		
+
+def index5(request):
+	template = loader.get_template('menu/index5.html')
+	return HttpResponse(template.render(request))
+
+def index6(request):
+	template = loader.get_template('menu/index6.html')
 	return HttpResponse(template.render(request))
 
 def module(request):
